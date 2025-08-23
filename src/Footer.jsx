19ef59420c5be2bb8aabc09512299675973746be
vanishcode <vanishcodex@email.com>
2025-08-23 21:08:42 +0330

    mobile update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full mt-10 text-center py-4 text-xs text-gray-500 bg-gray-50 border-t border-gray-200">
+    <footer className="w-full md:mt-10 mt-14 text-center py-4 text-xs text-gray-500 bg-gray-50 border-t border-gray-200">
       <p>© {currentYear} All rights reserved .</p>
       <p>Developed by  <span className="font-medium text-gray-700">Amir Davarzani</span></p>
       <p>
