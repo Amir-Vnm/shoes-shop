4c1beae9df4157be35d1521db35361009803034d
vanishcode <vanishcodex@email.com>
2025-08-23 20:57:11 +0330

    about update

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact() {
     return (
         <>
 
-        <h1 className="text-center bg-gray-700 text-white mt-15 mx-auto" >@amir.davarzannii@gmail.com</h1>
+        <h1 className="text-center bg-gray-700 text-white mt-15 mx-auto mb-[40%]" >@amir.davarzannii@gmail.com</h1>
         
     
         </>
